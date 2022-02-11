94d861f8dc008c92a0cace4544a94165c4478c42
Tomi <tomiwest@hotmail.com>
2022-02-11 15:46:20 +0200

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,30 @@ import React, { useState } from 'react'
 
 const Person = (props) => {
   console.log(props.name)
-  return <div>{props.name}</div>
+  return (
+    <div>
+      {props.name} {props.number}
+    </div>
+  )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567', id: 1 },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     if (!checkDuplicates()) {
       const personObject = {
         name: newName,
+        number: newNumber,
       }
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -33,6 +42,10 @@ const App = () => {
     //console.log(event.target.value)
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) => {
+    console.log('number change')
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
@@ -42,13 +55,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number:{' '}
+          <input value={newNumber} onChange={handleNumberChange}></input>
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person, key) => (
-          <Person key={key} name={person.name} />
+          <Person key={key} name={person.name} number={person.number} />
         ))}
       </div>
     </div>
