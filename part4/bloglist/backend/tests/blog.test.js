c4210633d979b6ee6308c08513f4562311a4b260
Tomi <tomiwest@hotmail.com>
2022-03-06 18:18:04 +0200

    4.4: helper functions and unit tests, step2

--- part4/bloglist/backend/tests/blog.test.js
+++ part4/bloglist/backend/tests/blog.test.js
@@ -1,3 +1,4 @@
+const blog = require('../models/blog')
 const listHelper = require('../utils/list_helper')
 
 test('dummy return one', () => {
@@ -5,4 +6,19 @@ test('dummy return one', () => {
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('dummy get total likes', () => {
+  const blogs = [
+    {
+      title: 'Blog Title',
+      author: 'blog Author',
+      url: 'blog url',
+      likes: 5,
+    },
+  ]
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(5)
+  })
 })
--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const initial = 0
+  return blogs.reduce((previous, { likes }) => previous + likes, initial)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
