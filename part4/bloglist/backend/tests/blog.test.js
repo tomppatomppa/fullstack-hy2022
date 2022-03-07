c568327ed2a8cb5f86ddf2b2d2df030a79fd4581
Tomi <tomiwest@hotmail.com>
2022-03-07 10:56:11 +0200

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/backend/tests/blog.test.js
+++ part4/bloglist/backend/tests/blog.test.js
@@ -1,4 +1,3 @@
-const blog = require('../models/blog')
 const listHelper = require('../utils/list_helper')
 
 test('dummy return one', () => {
@@ -20,5 +19,33 @@ describe('dummy get total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(5)
+  })
+})
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      title: 'Blog 1',
+      author: 'blog Author',
+      url: 'blog url',
+      likes: 5,
+    },
+    {
+      title: 'Blog 2',
+      author: 'blog Author',
+      url: 'blog url',
+      likes: 7,
+    },
+    {
+      title: 'Blog 3',
+      author: 'blog Author',
+      url: 'blog url',
+      likes: 1,
+    },
+  ]
+  test('return favorite', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    console.log(result)
+    expect(result).toEqual(blogs[1])
   })
 })
--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -7,7 +7,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((previous, { likes }) => previous + likes, initial)
 }
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce(
+    (prev, curr) => (prev = prev.likes > curr.likes ? prev : curr)
+  )
+
+  return favorite
+}
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
